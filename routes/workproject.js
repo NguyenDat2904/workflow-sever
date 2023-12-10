cea5a5a7991524c789179f55253e23537a70c412
Hung <vanhungnvh1712004@nvh1712004gmail.com>
2023-12-10 23:28:47 +0700

    feat/wf-14-data-profile

--- routes/workproject.js
+++ routes/workproject.js
@@ -5,7 +5,7 @@ const authMidddlerware = require('../middlerwares/authMiddleware');
 const refreshTokenMiddlerware = require('../middlerwares/refreshTokenMiddleware');
 
 router.get("/workdetail",refreshTokenMiddlerware,authMidddlerware,getdataproject.getWorkDetail)
-router.get("/listwork",refreshTokenMiddlerware,authMidddlerware,getdataproject.getListWork)
-router.get("/project/:_id",refreshTokenMiddlerware,authMidddlerware,getdataproject.getWorkProject)
+router.post("/listwork",refreshTokenMiddlerware,authMidddlerware,getdataproject.getListWork)
+router.post("/project/:_id",refreshTokenMiddlerware,authMidddlerware,getdataproject.getWorkProject)
 
 module.exports = router;
