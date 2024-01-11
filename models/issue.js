4e2ba8257934241177fb15e73d29b9e40fee819c
Nguyễn Văn Đạt <55286722+NguyenDat2904@users.noreply.github.com>
2024-01-11 09:44:10 +0700

    Modal Issue

--- models/issue.js
+++ models/issue.js
@@ -11,7 +11,7 @@ const IssueModel = new Schema(
         assignee: { type: String, ref: 'users' },
         reporter: { type: mongoose.Types.ObjectId, ref: 'users' },
         priority: { type: String, enum: ['Highest', 'High', 'Low', 'Lowest', 'Medium'], default: 'Medium' },
-        sprint: { type: mongoose.Types.ObjectId, ref: 'sprints' },
+        sprint: { type: String, ref: 'sprints' },
         storyPointEstimate: { type: Number },
         startDate: { type: Date },
         dueDate: { type: Date },
